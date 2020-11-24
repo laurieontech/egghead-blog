7dda83d4eba62a3ddf949696171fe8ea7d1fd657
Laurie <laurie@gatsbyjs.com>
2020-11-24 10:15:17 -0500

    Add a post page displaying the post title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 
 module.exports = {
     plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/posts`,
+                name: `posts`
+            }
+        },
         `gatsby-plugin-mdx`
     ]
 }
