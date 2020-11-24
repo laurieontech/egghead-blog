baa82e857631bf6dbb9093c1e4d37d9f6f3cba8d
Laurie <laurie@gatsbyjs.com>
2020-11-24 10:32:39 -0500

    Add listing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,33 @@
 import React from "react"
 import Layout from "../components/layout"
+import {graphql, Link} from "gatsby"
 
-const HomePage = () => ( 
+
+const HomePage = ({data}) => {
+    const posts = data.allMdx.nodes
+    return (
     <Layout>
-        <div> Hello World </div>
+        {posts.map((post) => {
+            return (
+                <Link to={`posts/${post.slug}`} key={post.slug}>
+                    <h2>{post.frontmatter.title}</h2>
+                </Link>
+            )}
+        )}
     </Layout>
-)
+)}
+
+export const pageQuery = graphql`
+query { 
+    allMdx(sort: { fields: [frontmatter___title], order: ASC }) {
+    nodes {
+        slug
+        frontmatter {
+            title
+        }
+    }
+  }
+}
+`
 
 export default HomePage
