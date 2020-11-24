a1849c11335ebf02540527e3682275ee01c48102
Laurie <laurie@gatsbyjs.com>
2020-11-24 10:18:41 -0500

    Add MDXRenderer and post body

--- src/pages/posts/{Mdx.parent__(File)__name}.js
+++ src/pages/posts/{Mdx.parent__(File)__name}.js
@@ -1,13 +1,24 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../../components/layout"
 
-const BlogPostPage = ({data}) => (
-    <h2>{data.mdx.frontmatter.title}</h2>
-)
+const BlogPostPage = ({data}) => {
+  const post = data.mdx
+  return (
+    <Layout>
+      <article>
+        <h2>{post.frontmatter.title}</h2>
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </article>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query BlogPostById($id: String) {
     mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
       }
