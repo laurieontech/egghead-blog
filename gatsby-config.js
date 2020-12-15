df4405cca4c02ca702e3897a97ef0df73c2cc9c4
Laurie <laurie@gatsbyjs.com>
2020-12-15 16:32:39 -0500

    add config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,27 @@
-
 module.exports = {
-    siteMetadata: {
-        title: `Egghead Blog`,
-        siteUrl: `https://example.com`
+  siteMetadata: {
+    title: `Egghead Blog`,
+    siteUrl: `https://example.com`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/posts`,
+        name: `posts`,
+      },
     },
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/posts`,
-                name: `posts`
-            }
-        },
-        `gatsby-plugin-mdx`,
-        {
-            resolve: `gatsby-plugin-theme-ui`,
-            options: {
-                prismPreset: `prism-okaidia`,
-                preset: `@theme-ui/preset-funk`
-            }
-        },
-        `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-react-helmet`,
-    ]
-}
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-theme-ui`,
+      options: {
+        prismPreset: `prism-okaidia`,
+        preset: `@theme-ui/preset-funk`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
+};
